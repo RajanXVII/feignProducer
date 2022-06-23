20ccba56b6f0ac32801d654593531c0b7cd5b27f
'RajanXVII' <a839348@DESKTOP-DN1RNVP>
2022-06-23 21:26:07 +0530

    Removing unnecessary dependencies

--- src/main/java/com/example/demo/FeignProducerApplication.java
+++ src/main/java/com/example/demo/FeignProducerApplication.java
@@ -5,20 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class FeignProducerApplication extends WebSecurityConfigurerAdapter{
+public class FeignProducerApplication {
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
-	}
+	
 
 	
 	public static void main(String[] args) {
